34476ece061a163ca5ecbf33adc9325452514990
Shikhar Khandelwal <shikhark@Shikhars-MacBook-Pro.local>
2018-09-01 20:30:31 +0800

    Succesful Register user Web-Request Handled, SCAR

---
+++ app/src/main/java/com/example/shikhark/chatapppersonal/services/AuthService.kt
@@ -0,0 +1,43 @@
+package com.example.shikhark.chatapppersonal.services
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.shikhark.chatapppersonal.utils.URL_REGISTER
+import org.jetbrains.anko.toast
+import org.json.JSONObject
+
+object AuthService {
+    fun registerUser(context: Context, email:String,password:String,complete:(Boolean)->Unit){
+        val url= URL_REGISTER
+        //creating JSON body
+        val jsonBody=JSONObject()
+        jsonBody.put("email",email)
+        jsonBody.put("password",password)
+
+        val requestBody=jsonBody.toString()
+
+
+        val registerRequest=object:StringRequest(Request.Method.POST,url, Response.Listener {_->
+            println("SUCCESS: User successfully created")
+            complete(true)
+        },Response.ErrorListener{error->
+            complete(false)
+            println("Error: Could not register User $error")
+        }){
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+        }
+
+        Volley.newRequestQueue(context).add(registerRequest)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/shikhark/chatapppersonal/utils/constants.kt
@@ -0,0 +1,4 @@
+package com.example.shikhark.chatapppersonal.utils
+
+const val BASE_URL="https://chatterchatter13.herokuapp.com/v1"
+const val URL_REGISTER="$BASE_URL/account/register"
