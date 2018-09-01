b2de4799a0d49c90809d1a1bd2f9f87c683cb786
Shikhar Khandelwal <shikhark@Shikhars-MacBook-Pro.local>
2018-09-01 22:17:16 +0800

    Added local host

--- app/src/main/java/com/example/shikhark/chatapppersonal/utils/constants.kt
+++ app/src/main/java/com/example/shikhark/chatapppersonal/utils/constants.kt
@@ -1,4 +1,5 @@
 package com.example.shikhark.chatapppersonal.utils
 
-const val BASE_URL="https://chatterchatter13.herokuapp.com/v1"
+//const val BASE_URL="https://chatterchatter13.herokuapp.com/v1"
+const val BASE_URL="http://10.0.2.2:3005/v1"
 const val URL_REGISTER="$BASE_URL/account/register"
