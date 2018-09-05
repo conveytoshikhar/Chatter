ebd0f8534736a0ac6fc4108249e27920c08e7e1a
Shikhar Khandelwal <shikhark@Shikhars-MacBook-Pro.local>
2018-09-05 16:46:23 +0800

    Added Loading Button

--- app/src/main/java/com/example/shikhark/chatapppersonal/controller/SignUpOpening.kt
+++ app/src/main/java/com/example/shikhark/chatapppersonal/controller/SignUpOpening.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shikhark.chatapppersonal.R
 import com.example.shikhark.chatapppersonal.utils.loadFonts
+import io.rmiri.buttonloading.ButtonLoading
+import kotlinx.android.synthetic.main.activity_sign_up_opening.*
+
 
 class SignUpOpening : AppCompatActivity() {
 
@@ -11,5 +14,19 @@ class SignUpOpening : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_opening)
         this.loadFonts("Raleway-Light.ttf")
+
+        proceed.setOnButtonLoadingListener(object : ButtonLoading.OnButtonLoadingListener {
+            override fun onClick() {
+                //...
+            }
+
+            override fun onStart() {
+                //...
+            }
+
+            override fun onFinish() {
+                //...
+            }
+        })
     }
 }
